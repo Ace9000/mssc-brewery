177d457260901a2661066a3ac678fd4e8f4a33ca
Anastas Acevski <anastas.acevski@outlook.com>
2023-06-12 14:53:29 +0200

    Initial commit

---
+++ src/main/java/springframework/msscbrewery/MsscBreweryApplication.java
@@ -0,0 +1,13 @@
+package springframework.msscbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springframework/msscbrewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package springframework.msscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
